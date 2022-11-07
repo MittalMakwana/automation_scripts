aa4c4a3644e5ed6882bec81885d073a1cf3bb4ae
Mittal Makwana <mittalmak@gmail.com>
2022-11-07 12:25:41 -0800

    Added new support

--- greasemonkey/passexe_raw_text.js
+++ greasemonkey/passexe_raw_text.js
@@ -1,10 +1,13 @@
 // ==UserScript==
-// @name         Text editor
+// @name         pastexe.com bypass
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Script to just view the text content of the links
+// @downloadURL  https://raw.githubusercontent.com/MittalMakwana/automation_scripts/master/greasemonkey/passexe_raw_text.js
+// @updateURL    https://raw.githubusercontent.com/MittalMakwana/automation_scripts/master/greasemonkey/passexe_raw_text.js
+// @version      0.3
+// @description  try to take over the world!
 // @author       You
 // @match        https://pastexe.com/*
+// @match        https://pastecanyon.com/*
 // @match        https://justetext.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pastexe.com
 // @grant        none
